838aa34b5d2b5d8b5ba9a65c52be4c0e1984b69c
lemaitre <9721016+lemaitre@users.noreply.github.com>
2023-12-03 21:33:32 +0100

    setup 2023

---
+++ 2023/shell.nix
@@ -0,0 +1,8 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  # rustup
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+
+  packages = with pkgs; [ bashInteractive rustc rustfmt rust-analyzer cargo clippy vscode ];
+}
