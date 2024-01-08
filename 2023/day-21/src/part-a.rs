cf8c074a91e6f0325d09ec5412f9032eb8fd2232
lemaitre <9721016+lemaitre@users.noreply.github.com>
2024-01-08 22:08:18 +0100

    2023-21-a optim

--- 2023/day-21/src/part-a.rs
+++ 2023/day-21/src/part-a.rs
@@ -2,7 +2,6 @@ use std::time::Instant;
 
 use anyhow::Result;
 use aoc_lib::ascii::{AsAsciiStr, AsciiChar, AsciiStr};
-use bitvec::{bitarr, BitArr};
 
 fn main() {
     let filename = std::env::args().nth(1).unwrap_or("input.txt".to_string());
@@ -13,7 +12,8 @@ fn main() {
     solve(content).expect("Could not solve part A")
 }
 
-type Bits = BitArr!(for 192);
+const N: usize = 3;
+type Bits = [u64; N];
 
 fn solve(input: &AsciiStr) -> Result<()> {
     let timer = Instant::now();
@@ -21,20 +21,32 @@ fn solve(input: &AsciiStr) -> Result<()> {
     let mut grid0 = obstacles.clone();
 
     for line in input.lines() {
-        let mut obstacles_row = bitarr!(1; 192);
-        let mut row = bitarr!(0; 192);
+        let mut obstacles_row = [0_u64; N];
+        let mut row = [0_u64; N];
 
-        for (j, &chr) in line.into_iter().enumerate() {
-            obstacles_row.set(j, chr == AsciiChar::Hash);
-            row.set(j, chr == AsciiChar::S);
+        let mut chars = line.into_iter();
+
+        for i in 0.. {
+            let mut j = 0_usize;
+            let mut cell = 0_u64;
+            let mut obstacle_cell = 0_u64;
+
+            for &chr in chars.by_ref().take(64) {
+                obstacle_cell |= ((chr == AsciiChar::Hash) as u64) << j;
+                cell |= ((chr == AsciiChar::S) as u64) << j;
+                j += 1;
+            }
+            obstacles_row[i] = obstacle_cell;
+            row[i] = cell;
+            if j < 64 {
+                break;
+            }
         }
 
         obstacles.push(obstacles_row);
         grid0.push(row);
     }
-    let mut grid1 = (0..grid0.len())
-        .map(|_| bitarr!(0; 192))
-        .collect::<Vec<_>>();
+    let mut grid1 = (0..grid0.len()).map(|_| [0_u64; N]).collect::<Vec<_>>();
 
     for _ in 0..64 {
         std::mem::swap(&mut grid0, &mut grid1);
@@ -46,11 +58,21 @@ fn solve(input: &AsciiStr) -> Result<()> {
             .zip(grid1[2..].iter())
             .zip(obstacles[1..].iter())
         {
-            let mut left = *current;
-            let mut right = *current;
-            left.shift_left(1);
-            right.shift_right(1);
-            *row = (left | right | previous | next) & !*obstacle_row;
+            let mut left = 0_u64;
+            let mut center = current[0];
+            for j in 0..N - 1 {
+                let right = current[j + 1];
+                let left1 = (center << 1) | (left >> 63);
+                let right1 = (center >> 1) | (right << 63);
+                row[j] = (left1 | right1 | previous[j] | next[j]) & !obstacle_row[j];
+                left = center;
+                center = right;
+            }
+            let j = N - 1;
+            let right = 0_u64;
+            let left1 = (center << 1) | (left >> 63);
+            let right1 = (center >> 1) | (right << 63);
+            row[j] = (left1 | right1 | previous[j] | next[j]) & !obstacle_row[j];
 
             previous = current;
             current = next;
@@ -58,8 +80,10 @@ fn solve(input: &AsciiStr) -> Result<()> {
     }
 
     let mut s = 0;
-    for row in grid0 {
-        s += row.count_ones();
+    for row in &grid0 {
+        for cell in row {
+            s += cell.count_ones();
+        }
     }
 
     let elapsed = timer.elapsed();
