8965422041c4bd56934400ac4d9ad6dd6b59b914
lemaitre <9721016+lemaitre@users.noreply.github.com>
2024-12-14 18:03:03 +0100

    2024-00

---
+++ 2024/day-00/part-b.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+
+import click
+
+@click.command()
+@click.argument('input', type=click.File('r'))
+def part_b(input: click.File):
+  for line in input.readlines():
+    click.echo(line)
+
+if __name__ == '__main__':
+  part_b()
